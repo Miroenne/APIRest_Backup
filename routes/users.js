68eac7d63804f8d576e81c70aaec5bf7a00d80f6
Miroenne <mmenin8306@icloud.com>
2026-02-10 19:58:43 +0100

    Added middlewares directory and private.js file, added changes in routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const service = require("../services/users");
+const private = require("../middlewares/private");
 
-router.get("/:id", service.getById); /* lire les infos */
+router.get("/:id", private.checkJWT, service.getById); /* lire les infos */
 router.put("/add", service.add); /* ajouter un utilisateur */ 
-router.patch("/:id", service.update); /* modifier un utilisateur */
-router.delete("/:id", service.delete); /* supprimer un utilisateur */
+router.patch("/:id", private.checkJWT, service.update); /* modifier un utilisateur */
+router.delete("/:id", private.checkJWT, service.delete); /* supprimer un utilisateur */
 
 router.post('/authenticate', service.authenticate); /* authentification d'un utilisateur */
 
