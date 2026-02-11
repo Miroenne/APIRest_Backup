6b0e0b664114073c701ac05528314ea0829df185
Miroenne <mmenin8306@icloud.com>
2026-02-11 13:25:53 +0100

    Added the file uploading files and the view files

---
+++ services/files.js
@@ -0,0 +1,18 @@
+const {storage} = require('../middlewares/files-storage');
+const File = require('../models/file');
+const fs = require('fs');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+exports.createOneFile = (req, res, next) => {
+    const file = new File({
+        name: req.file.originalname,
+        imageUrl: '${req.protocol}://localhost:3000/uploads/${req.file.filename}',
+        userId: req.body.userId
+    });
+
+    file.save()
+        .then(() => res.status(201).json({message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({error}));
+};
